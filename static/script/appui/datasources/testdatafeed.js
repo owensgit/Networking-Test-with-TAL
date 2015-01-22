86ffa64d7951c0016e2e4805c78eeccc4aab9a40
Owen Evans <owensinbox@gmail.com>
2015-01-22 12:18:19 +0000

    Fixed networking

--- static/script/appui/datasources/testdatafeed.js
+++ static/script/appui/datasources/testdatafeed.js
@@ -1,20 +1,19 @@
 require.def("basicapp/appui/datasources/testdatafeed",
     [
         "antie/class",
-        "antie/devices/device" // is this correct?
+        "antie/runtimecontext" // pass in the runtime context class
     ],
-    function(Class, Device) {
+    function(Class, RuntimeContext) {
         return Class.extend({
             loadData : function(callbacks) {
 
                 var url = 'http://localhost:8888/network_test_tal/test_data.json'; 
-                var device = new Device(); // is this correct?
+                var device = RuntimeContext.getDevice(); // get the device object via the application class
 
                 device.loadURL(url, {
                     onLoad: function(responseText) {
-                        console.log("Success!", responseText);
-
-                        callbacks.onSuccess(responseText);
+                        // run the callback onSuccess method to apply the data
+                        callbacks.onSuccess(JSON.parse(responseText));
                     },
                     onError: function(responseText) {
                         console.error("Error!", responseText);
